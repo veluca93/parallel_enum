e4cb32794d9a4b3a1993f71beb0985de12491be6
Luca Versari <veluca93@gmail.com>
2018-01-25 20:46:12 +0100

    Fix

--- util/bitset.hpp
+++ util/bitset.hpp
@@ -45,19 +45,55 @@ class bitset {
 
   void list(std::vector<size_t>* out, size_t limit = 0) const {
     size_t cnt = 0;
+    iterator it = begin();
     for (size_t i = 0; i < SZ; i++) {
       uint64_t curr = data_[i];
       while (curr != 0) {
         size_t off = __builtin_ctzll(curr);
+        ++it;
         out->push_back(i * bits_per_item + off);
         cnt++;
         if (cnt == limit) return;
         curr &= ~(1ULL << off);
       }
     }
+    if (it != end()) throw std::runtime_error("IT too long");
   }
 
-  /*
+  class iterator {
+   public:
+    iterator(const bitset* parent) : parent(parent) {
+      if (!parent->get(0)) ++(*this);
+    }
+    iterator(const bitset* parent, size_t start)
+        : curr(start), parent(parent) {}
+
+    size_t operator*() const { return curr; }
+    bool operator!=(const iterator& other) { return curr != other.curr; }
+    iterator& operator++() {
+      size_t off_start = (curr + 1) % bitset::bits_per_item;
+      for (size_t i = (curr + 1) / bitset::bits_per_item; i < SZ; i++) {
+        uint64_t d = parent->data_[i] >> off_start;
+        if (d != 0) {
+          size_t off = __builtin_ctzll(d);
+          curr = i * bitset::bits_per_item + off + off_start;
+          return *this;
+        }
+        off_start = 0;
+      }
+      curr = SZ * bitset::bits_per_item;
+      return *this;
+    }
+
+   private:
+    size_t curr = 0;
+    const bitset* parent;
+  };
+  friend class iterator;
+
+  iterator begin() const { return iterator(this); }
+  iterator end() const { return iterator(this, SZ * bits_per_item); }
+
   size_t front() const {
     for (size_t i = 0; i < SZ; i++) {
       uint64_t curr = data_[i];
@@ -66,6 +102,7 @@ class bitset {
         return i * bits_per_item + off;
       }
     }
+    return SZ * bitset::bits_per_item;
   }
 
   size_t back() const {
@@ -76,21 +113,22 @@ class bitset {
         return (i - 1) * bits_per_item + off;
       }
     }
-  }*/
+    return SZ * bitset::bits_per_item;
+  }
 
-  bitset& operator|(const bitset& other) const {
+  bitset operator|(const bitset& other) const {
     bitset ans;
     for (size_t i = 0; i < SZ; i++) ans.data_[i] = data_[i] | other.data_[i];
     return ans;
   }
 
-  bitset& operator&(const bitset& other) const {
+  bitset operator&(const bitset& other) const {
     bitset ans;
     for (size_t i = 0; i < SZ; i++) ans.data_[i] = data_[i] & other.data_[i];
     return ans;
   }
 
-  bitset& operator-(const bitset& other) const {
+  bitset operator-(const bitset& other) const {
     bitset ans;
     for (size_t i = 0; i < SZ; i++) ans.data_[i] = data_[i] & ~other.data_[i];
     return ans;
